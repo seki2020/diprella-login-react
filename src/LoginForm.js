14c2250a420a3d4b42542fbcfbb6dab2db5e531a
Jason <v8main@gmail.com>
2019-07-18 19:20:41 +0900

    Best pratice

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -11,10 +11,11 @@ class LoginForm extends React.Component {
             }
         }
 
-        this.maskRef = React.createRef()
+        this.container = React.createRef()
+        // this.signUpBtn = React.createRef()
 
-        this.MoveRight = this.MoveRight.bind(this)
-        this.MoveLeft = this.MoveLeft.bind(this)
+        this.onSignIn = this.onSignIn.bind(this)
+        this.onSignUp = this.onSignUp.bind(this)
 
     }
     componentDidMount() {
@@ -38,97 +39,118 @@ class LoginForm extends React.Component {
 
     }
 
-    
-
-    MoveRight() {
-        clearInterval(this.interval);
-
-        let numLeft = 512
-        let numRight = 0
-        this.interval = setInterval(() => {
-            numLeft = numLeft - 10
-            numRight = numRight + 10
-            if (numLeft < -10) {
-                clearInterval(this.interval);
-                return
-            }
-
-            this.setState({ maskStyle: { left: numLeft, right: numRight } })
-        }, 5);
+    onSignUp() {
+        this.container.current.classList.add("right-panel-active");
     }
-    MoveLeft() {
-        clearInterval(this.interval);
-
-        let numLeft = 0
-        let numRight = 512
-        this.interval = setInterval(() => {
-            numLeft = numLeft +10
-            numRight = numRight - 10
-            if (numRight < -10) {
-                clearInterval(this.interval);
-                return
-            }
-
-            this.setState({ maskStyle: { left: numLeft, right: numRight } })
-        }, 5);
+    onSignIn() {
+        debugger
+        this.container.current.classList.remove("right-panel-active");
     }
+
+    // MoveRight() {
+    //     clearInterval(this.interval);
+
+    //     let numLeft = 512
+    //     let numRight = 0
+    //     this.interval = setInterval(() => {
+    //         if (numLeft <= 0) {
+    //             clearInterval(this.interval);
+    //             return
+    //         }
+
+    //         let seedLeft = 10;
+    //         let seedRight = 10;
+    //         if (numLeft > 400) {
+    //             seedLeft = 10
+    //             seedRight = 5
+    //         } else {
+    //             seedLeft = 5
+    //             seedRight = 10
+    //         }
+
+
+    //         numLeft = numLeft - seedLeft
+    //         numRight = numRight + seedRight
+
+
+    //         this.setState({ maskStyle: { left: numLeft, right: numRight } })
+    //     }, 20);
+    // }
+    // MoveLeft() {
+    //     clearInterval(this.interval);
+
+    //     let numLeft = 0
+    //     let numRight = 512
+    //     this.interval = setInterval(() => {
+
+    //         if (numRight <= 0) {
+    //             clearInterval(this.interval);
+    //             return
+    //         }
+
+    //         numLeft = numLeft + 10
+    //         numRight = numRight - 10
+
+    //         this.setState({ maskStyle: { left: numLeft, right: numRight } })
+    //     }, 20);
+    // }
 
     render() {
         const clipValue = `inset(0 ${this.state.maskStyle.left}px 0 ${this.state.maskStyle.right}px)`
         // const clipValue =  `inset(0 200px 0 200px)`
 
         return (
-            <div className="container">
-                <div className="card-container" ref={this.maskRef}
-                    style={{ 'zIndex': '9999', 'clipPath': clipValue }}>
-                    <div className=" left aqua">
-                        <h1 className="card-header">Welcome Back</h1>
-                        <button onClick={this.MoveRight}>submit</button>
-
-                    </div>
-                    <div className="center-empty"></div>
-                    <div className="aqua  right">
-                        <h1 className="card-header">Hello Friend</h1>
-                        <button onClick={this.MoveLeft}>submit</button>
+            <div className="container" id="container" ref={this.container}>
+                <div className="form-container sign-up-container">
+                    <form action="#">
+                        <h1>Create Account</h1>
+                        <div className="social-container">
+                            <a href="#" className="social">
+                                <i className="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="#" className="social">
+                                <i className="fab fa-google-plus-g"></i>
+                            </a>
+                            <a href="#" className="social">
+                                <i className="fab fa-linkedin-in"></i>
+                            </a>
+                        </div>
+                        <span>or use your email for registration</span>
+                        <input type="text" placeholder="Name" />
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <button onClick={this.onSignUp}>Sign Up</button>
+                    </form>
+                </div>
+                <div className="form-container sign-in-container">
+                    <form action="#">
+                        <h1>Sign in</h1>
+                        <div className="social-container">
+                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+                        </div>
+                        <span>or use your account</span>
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <a href="#">Forgot your password?</a>
+                        <button onClick={this.onSignIn}>Sign In</button>
+                    </form>
+                </div>
+                <div className="overlay-container">
+                    <div className="overlay">
+                        <div className="overlay-panel overlay-left">
+                            <h1>Welcome Back!</h1>
+                            <p>To keep connected with us please login with your personal info</p>
+                            <button className="ghost" id="signIn" onClick={this.onSignIn}>Sign In</button>
+                        </div>
+                        <div className="overlay-panel overlay-right">
+                            <h1>Hello, Friend!</h1>
+                            <p>Enter your personal details and start journey with us</p>
+                            <button className="ghost" id="signUp" onClick={this.onSignUp}>Sign Up</button>
+                        </div>
                     </div>
                 </div>
-                <div className="card-container" style={{'zIndex':this.state.maskStyle.right}}>
-                    <div className="card left">
-                        <h1 className="card-header">Create Account</h1>
-                        <form className="card-content">
-                            <div>
-                                <label>Username</label>
-                                <input type="text" placeholder="type in usename" />
-                            </div>
-                            <div>
-                                <label>Password</label>
-                                <input type="password" placeholder="type in usename" />
-                            </div>
-                        </form>
-                    </div>
-                    <div className="card-empty right">
-                        {/* placeholder */}
-                    </div>
-                </div>
-                <div className="card-container" style={{'zIndex':this.state.maskStyle.left}}>
-                    <div className=" left card-empty">
-                        {/* placeholder */}
-                    </div>
-                    <div className="card right">
-                        <h1 className="card-header">Sign in to Diprella</h1>
-                        <form className="card-content">
-                            <div>
-                                <label>Username</label>
-                                <input type="text" placeholder="type in usename" />
-                            </div>
-                            <div>
-                                <label>Password</label>
-                                <input type="password" placeholder="type in usename" />
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
             </div>
         )
     }
